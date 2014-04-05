b92ce92c6f69083d6c03c6ba1ebc3f87a3b58131
jasiekmiko <jasiekmiko@users.noreply.github.com>
2014-04-05 03:07:53 +0200

    Applications skeletons
    Created the IntelliJ projects with applications and libary studs.

---
+++ Lectures+ Lecturer/src/com/example/Lectures__Lecturer/LecturerMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.Lectures__Lecturer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LecturerMainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

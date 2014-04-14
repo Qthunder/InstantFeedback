470a96039876d9d811e0031a6b116b3afedae62a
jasiekmiko <jasiekmiko@users.noreply.github.com>
2014-04-14 23:04:42 +0200

    Lecture class placeholder
    Lecture class created for development use in the rest of the program.
    Needs to be implemented with its actual mechanics

---
+++ Lectures+ Student/src/com/example/Lectures__Student/Lecture.java
@@ -0,0 +1,16 @@
+package com.example.Lectures__Student;
+
+import android.net.nsd.NsdServiceInfo;
+
+/**
+ * Created by Jasiek on 14/04/2014.
+ */
+public class Lecture {
+    //TODO - whole class is only a placeholder right now
+
+    private NsdServiceInfo lectureInfo;
+
+    public Lecture(NsdServiceInfo serviceInfo) {
+        lectureInfo = serviceInfo;
+    }
+}

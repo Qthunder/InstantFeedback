aa869dd9b7971ab8e6b44163a52ea29b035608f3
Edisach <level007@gmail.com>
2014-04-16 21:49:03 +0100

    select count for lectures and questions

--- Lectures+ Lecturer/src/com/example/Lectures__Lecturer/DatabaseManager.java
+++ Lectures+ Lecturer/src/com/example/Lectures__Lecturer/DatabaseManager.java
@@ -20,6 +20,11 @@ public class DatabaseManager extends Application{
     private static final String decrementRank =
             "UPDATE ? SET rank = rank - 1 WHERE rank >= ? AND ? = ?";
 
+    private long getSize (String table_name, String id, Integer test) {
+        String temp = "SELECT number FROM table_name WHERE " + id + " = " + test;
+        return db.compileStatement(temp).simpleQueryForLong();
+    }
+
     private Integer getSize(String table, Integer id) {
         String sql = "SELECT size FROM " + table + "WHERE " + table + "_id = " + id;
         SQLiteStatement compiled = db.compileStatement(sql);
@@ -55,6 +60,10 @@ public class DatabaseManager extends Application{
             Lectures.COLUMN_NAME_COURSE_ID, course_id};
             db.execSQL(incrementRank, bindArgs);
         }
+        else {
+            long temp = getSize(Lectures.TABLE_NAME, Lectures.COLUMN_NAME_COURSE_ID, course_id);
+            values.put(Lectures.COLUMN_NAME_LECTURE_RANK, temp);
+        }
 
         Integer newRowId;
         newRowId = (int)(long)db.insert(Lectures.TABLE_NAME, null, values);
@@ -80,6 +89,10 @@ public class DatabaseManager extends Application{
             Object[] bindArgs = new Object[]{Questions.TABLE_NAME, Questions.COLUMN_NAME_QUESTION_RANK,
             Questions.COLUMN_NAME_LECTURE_ID, lecture_id};
             db.execSQL(incrementRank, bindArgs);
+        }
+        else {
+            long temp = getSize(Questions.TABLE_NAME, Questions.COLUMN_NAME_LECTURE_ID, lecture_id);
+            values.put(Questions.COLUMN_NAME_QUESTION_RANK, temp);
         }
 
         Integer newRowId;
@@ -110,7 +123,8 @@ public class DatabaseManager extends Application{
             db.execSQL(incrementRank, bindArgs);
         }
         else {
-            values.put(Answers.COLUMN_NAME_ANSWER_RANK, )
+            long temp = getSize(Answers.TABLE_NAME, Answers.COLUMN_NAME_QUESTION_ID, question_id);
+            values.put(Answers.COLUMN_NAME_ANSWER_RANK, temp);
         }
 
         Integer newRowId;

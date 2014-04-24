c253d461e12d8dabc1cc470e1728ced84afdd093
Amy Welch <aewelch@hotmail.co.uk>
2014-04-24 18:15:45 +0100

    Student Activity added with button bar

---
+++ Lectures+ Student/src/com/InstantFeedback/Student/StudentLectureActivity.java
@@ -0,0 +1,18 @@
+package com.InstantFeedback.Student;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Amy on 24/04/2014.
+ */
+public class StudentLectureActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lecture);
+
+
+    }
+}

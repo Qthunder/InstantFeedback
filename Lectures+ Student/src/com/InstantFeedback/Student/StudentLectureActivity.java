fd5414212a57b3730ff338a3a44fc0061b6509f0
Gabriel Asman <Gabriel Asman>
2014-05-06 16:17:21 +0100

    Gabriel1

--- Lectures+ Student/src/com/InstantFeedback/Student/StudentLectureActivity.java
+++ Lectures+ Student/src/com/InstantFeedback/Student/StudentLectureActivity.java
@@ -3,7 +3,9 @@ package com.InstantFeedback.Student;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Amy on 24/04/2014.
@@ -20,7 +22,25 @@ public class StudentLectureActivity extends Activity {
         Intent intent = getIntent();
         selectedLecturer = intent.getStringExtra("com.InstantFeedback.Student.SelectedLecturer");
 
-        TextView text = (TextView) findViewById(R.id.textView);
+        TextView text = (TextView) findViewById(R.id.UserName);
         text.setText("You are connected to " + selectedLecturer);
     }
+
+    public void goSlower(View view) {
+        Toast.makeText(this, "Go Slower !", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goFaster(View view) {
+        Toast.makeText(this, "Go Faster !",Toast.LENGTH_SHORT).show();
+    }
+
+    public void putQuestion(View view){
+        Toast.makeText(this,"Put Question "  , Toast.LENGTH_SHORT).show();
+    }
+    public void sendAnswer(View view){
+
+        Toast.makeText(this,"Sent answer "  , Toast.LENGTH_SHORT).show();
+
+    }
 }
+
